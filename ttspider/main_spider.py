e5300057ba94a29ea8036e0e22ad5f91fe39c6c6
chenleiwh <lei-chen_neu@neusoft.com>
2018-04-03 17:33:33 +0800

    获取作者和作者链接

--- ttspider/main_spider.py
+++ ttspider/main_spider.py
@@ -12,14 +12,10 @@ browser = webdriver.Firefox()
 
 def get_first_page():
     browser.get('https://www.toutiao.com/')
-    #elements = browser.find_elements_by_xpath('//a[contains(@href,"c/user")]')
-
-    #elements = browser.find_elements_by_xpath('//div[contains(@class,"bui-left footer-bar-left")]')
 
     elements = browser.find_elements_by_xpath('//div[@class="bui-left footer-bar-left"]')
 
     for i in elements:
-        #print(i.get_attribute('innerHTML'))
         html = i.get_attribute('innerHTML')
         soup = BeautifulSoup(html, "html.parser")
         name = soup.find(attrs={'ga_event':'article_name_click'})
