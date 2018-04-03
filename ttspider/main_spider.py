8df4d6b0b5a9131334a8bd717024ecdf27af75da
chenleiwh <lei-chen_neu@neusoft.com>
2018-04-03 17:25:06 +0800

    获取作者和作者链接

--- ttspider/main_spider.py
+++ ttspider/main_spider.py
@@ -6,12 +6,30 @@
 # @Date  : 2018/3/26
 
 from selenium import webdriver
+from bs4 import BeautifulSoup
 
 browser = webdriver.Firefox()
 
 def get_first_page():
     browser.get('https://www.toutiao.com/')
-    print(browser.find_elements("css selector",".bui-box single-mode"))
+    #elements = browser.find_elements_by_xpath('//a[contains(@href,"c/user")]')
+
+    #elements = browser.find_elements_by_xpath('//div[contains(@class,"bui-left footer-bar-left")]')
+
+    elements = browser.find_elements_by_xpath('//div[@class="bui-left footer-bar-left"]')
+
+    for i in elements:
+        #print(i.get_attribute('innerHTML'))
+        html = i.get_attribute('innerHTML')
+        soup = BeautifulSoup(html, "html.parser")
+        name = soup.find(attrs={'ga_event':'article_name_click'})
+        try:
+            print(name.get_text())
+            print(name["href"])
+        except:
+            continue
+
+    browser.close()
 
 if __name__=='__main__':
     get_first_page()
